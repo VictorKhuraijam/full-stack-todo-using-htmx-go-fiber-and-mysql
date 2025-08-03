d0e86869eae9a0b69ec2414cb5077ef3cbad25da
Victor Khuraijam <khuraijamvictor@gmail.com>
2025-08-03 11:09:17 +0530

    Comments added

--- main.go
+++ main.go
@@ -39,6 +39,8 @@ func main() {
 		Views: engine,
 	})
 
+
+
 	//Middleware
 	app.Use(logger.New())
 
--- server/database/database.go
+++ server/database/database.go
@@ -26,12 +26,18 @@ func createTable(db *sql.DB) {
 	}
 }
 
+//why pass a pointer not a value
+//duplicating and resetting internal state for every function call.
+//*sql.DB lets you share the same connection pool across your whole app.
+
 func Connect(cfg *config.Config) (*sql.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
         cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+	//dsn(Data Source Name) It’s a formatted connection string used to describe how to connect to your MySQL database.
 
-	fmt.Println(dsn)
-	db, err := sql.Open("mysql", dsn)
+	log.Println(dsn)
+	db, err := sql.Open("mysql", dsn) //creates a sql.DB struct(a wrapper)
+	//holds driver info, configs like max opens/idle connections, a pool of connections(lazy conncection managemenet)
 	if err != nil{
 		return nil,err
 	}
@@ -39,9 +45,12 @@ func Connect(cfg *config.Config) (*sql.DB, error) {
 	fmt.Println(db)
 	createTable(db)
 
-	if err = db.Ping(); err != nil{
+	if err := db.Ping(); err != nil{
 		return nil, err
 	}
 
 	return db, nil
+	//returns a pointer of the type *sql.DB which is safe for concurrent use
+	//You can share it across goroutines.
+  	//Under the hood, it uses mutexes and channels to manage access.
 }
